c3d3850d82556a3b2e37dec4bcb1d0498fbc57f3
Saksham jain <130843255+Jsakshxm@users.noreply.github.com>
2024-04-09 16:46:38 +0530

    cipher

--- src/app/SachkaSamna/page.js
+++ src/app/SachkaSamna/page.js
@@ -11,7 +11,7 @@ const Page = () => {
       <div className='grid grid-cols-1 gap-8 lg:grid-cols-3'>
      <Link href="/LCS">   <img className='drop-shadow-[0_0px_50px_rgb(216,18,93)] rounded-lg animate-pulse' src="eye.gif" alt="Eye Animation" /> </Link>
      <Link href="/MathsPremi">   <img className='drop-shadow-[0_0px_50px_rgb(216,18,93)] rounded-lg animate-pulse' src="eye.gif" alt="Eye Animation" /> </Link>
-     <Link href="/">   <img className='drop-shadow-[0_0px_50px_rgb(216,18,93)] rounded-lg animate-pulse' src="eye.gif" alt="Eye Animation" /> </Link>
+     <Link href="/SecretMessage">   <img className='drop-shadow-[0_0px_50px_rgb(216,18,93)] rounded-lg animate-pulse' src="eye.gif" alt="Eye Animation" /> </Link>
       </div>
     </div>
   );
