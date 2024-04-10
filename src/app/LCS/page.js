992066efee2c88cd7d0e56fc17b0fd3a229190fa
Saksham jain <130843255+Jsakshxm@users.noreply.github.com>
2024-04-10 23:19:26 +0530

    hi

--- src/app/LCS/page.js
+++ src/app/LCS/page.js
@@ -4,34 +4,34 @@ import { useState } from "react";
 
 const DragonGifWithText = () => {
   const [win, setWin] = useState(false);
-  const key = 38
+  const saksham = 38;
   const [id, setId] = useState(null);
-
+  
 
   const handleCheck = () => {
-    if (id == key) {
+    if (id == saksham) {
       setWin(true);
-    
     }
-    console.log(key,id);
   };
 
   return (
     <div className="relative h-screen bg-black">
-      <img src="/dragon.gif" alt="Small Dragon" className="absolute top-0 left-0 object-cover h-80 w-[44rem]" />
-
-      <div className="absolute bottom-0 right-0">
-        <img src="/dragon.gif" alt="Dragon" className="object-cover transform scale-x-[-1] h-80 w-[44rem]" />
+      <div className="absolute transform -translate-x-1/2 -translate-y-1/2 left-80 top-3/4">
+        <img src="/dragon.gif" alt="Small Dragon" className="object-cover h-96 w-[44rem]" />
       </div>
 
-      <div className="absolute" style={{ top: "220px", left: "50%", transform: 'translateX(-50%)' }}>
-        <p className="px-10 py-6 text-4xl font-bold text-red-600 shadow-2xl animate-flame shadow-red-700">
+      <div className="absolute top-0 right-0">
+        <img src="/dragon.gif" alt="Dragon" className="object-cover transform scale-x-[-1] h-[26rem] w-[44rem]" />
+      </div>
+
+      <div className="absolute transform -translate-x-1/2 top-52 left-1/4 w-72">
+        <p className="px-6 py-3 text-2xl font-bold text-white bg-black bg-opacity-50">
           "Do you sense the whispers of the ancients, brother? What tale do these ancient runes convey?"
         </p>
       </div>
 
-      <div className="absolute right-48 top-[610px]">
-        <p className="p-5 text-4xl font-bold text-red-600 shadow-2xl shadow-black animate-flame-right-to-left">
+      <div className="absolute bottom-12 right-12 w-80">
+        <p className="px-6 py-3 text-2xl font-bold text-white bg-black bg-opacity-50">
           "Aye, the winds carry the echoes of a forgotten saga, but can you decipher the riddles woven in these cryptic strings?"
         </p>
       </div>
@@ -49,10 +49,9 @@ const DragonGifWithText = () => {
         </button>
       </div>
 
-      {win && <p className="absolute px-6 py-3 text-2xl text-white transform -translate-x-1/2 bg-green-500 rounded-lg bottom-4 left-1/2">Congratulations! Output + 103073 = res ult</p>}
+      {win && <p className="absolute px-6 py-3 text-lg text-white bg-green-500 rounded-lg bottom-4 left-1/2">Congratulations! Output + 103073 = result</p>}
     </div>
   );
 };
 
 export default DragonGifWithText;
- 
