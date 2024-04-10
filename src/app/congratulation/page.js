9e7a3ddf4a4814cf7a9521b838529a64afa32eb6
Saksham jain <130843255+Jsakshxm@users.noreply.github.com>
2024-04-11 02:37:26 +0530

    error fix

---
+++ src/app/congratulation/page.js
@@ -0,0 +1,22 @@
+"use client";
+import React from 'react'
+import { useState, useEffect } from 'react'
+import JSConfetti from 'js-confetti'
+import Link from 'next/link'
+
+
+
+const page = () => {
+    useEffect(() => {
+        const jsConfetti = new JSConfetti()
+        jsConfetti.addConfetti();
+    },[]);
+  return (
+    <div className="flex flex-col items-center justify-between min-h-screen p-24">
+      <h1> congratulations bhai ! Abb aagle stage pr jane ka rasta thund </h1>
+      <Link href="Level2"><button className='p-10 bg-blue-500 opacity-0 '> Submit</button></Link>
+    </div>
+  )
+}
+
+export default page
